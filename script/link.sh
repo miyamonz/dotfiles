57b9a4e7392e4eab5f5f6a78763d545327549abb
Koichi Miyamoto <m5813k@gmail.com>
2017-11-11 01:00:31 +0900

    vimrcをneovim用にlnする

--- script/link.sh
+++ script/link.sh
@@ -10,4 +10,5 @@ for f in vim/.*?? git/.*?? tmux/.*?? ".bashrc" ".inputrc"
 do
     ln -snfv ${DOT_DIRECTORY}/${f} ${DST_DIRECTORY}/`basename $f`
 done
+ln -snfv ${DOT_DIRECTORY}/vim/.vimrc ${DST_DIRECTORY}/.config/nvim/init.vim
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
