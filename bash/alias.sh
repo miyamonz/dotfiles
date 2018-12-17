7942cf3be46258d198c023be33fb5f5a718520c8
Koichi Miyamoto <m5813k@gmail.com>
2018-12-17 17:31:31 +0900

    ghq とgopath srcの共存

--- bash/alias.sh
+++ bash/alias.sh
@@ -26,7 +26,7 @@ alias vim='nvim'
 alias arduino='/Applications/Arduino.app/Contents/MacOS/Arduino'
 
 function ghq-list() {
-    find $(ghq root) -d 3 -maxdepth 3 | grep -v DS_Store | sed -e "s#$(ghq root)/##g"
+    find $(ghq root --all | xargs echo) -d 3 -maxdepth 3 | grep -v DS_Store  | sed -e "s#$(echo ~)/##g"
 }
-alias r='cd $(ghq root)/$(ghq-list | peco)'
+alias r='cd ~/$(ghq-list | peco)'
 
