2dd68f75656b51887d6d9ea4588b657e15b0c5cc
Koichi Miyamoto <m5813k@gmail.com>
2019-03-12 18:12:14 +0900

    propmt修正

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,9 +1,7 @@
-WHITE="\[\e[00m\]"
-GREEN="\[\e[32m\]"
-BLUE="\[\e[34m\]"
-RED="\[\e[31m\]"
-
 function _ps1_result() {
+    # printfには\[\]使わない
+    local BLUE="\e[34m"
+    local RED="\e[31m"
     local code=$?
     if [[ ${code} == 0 ]]; then
         local kao="(っ'ω')っ"
@@ -14,9 +12,14 @@ function _ps1_result() {
     fi
     echo ;
 }
+export PROMPT_COMMAND="_ps1_result"
 
+WHITE="\[\e[00m\]"
+GREEN="\[\e[32m\]"
+BLUE="\[\e[34m\]"
+RED="\[\e[31m\]"
+# miyamonz@pcname:path (git)$
 PS_USER="${GREEN}\u@\h"
 PS_DIR="${BLUE}\w"
 PS_GIT="${RED}\$(__git_ps1)"
-export PROMPT_COMMAND="_ps1_result"
 export PS1="${PS_USER}${WHITE}:${PS_DIR}${PS_GIT}${WHITE}\$ "
