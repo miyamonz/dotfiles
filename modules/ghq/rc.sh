abacf14595d3e154b751fcbb3d9fbd845d209bf0
Koichi Miyamoto <m5813k@gmail.com>
2020-12-24 03:26:52 +0900

    fix ghq setting

--- modules/ghq/rc.sh
+++ modules/ghq/rc.sh
@@ -1,5 +1,5 @@
 ghq-list() {
-    find $(ghq root --all | xargs echo) -d 3 -maxdepth 3 | grep -v DS_Store  | sed -e "s#$(echo ~)/##g"
+    find $(ghq root --all | xargs echo) -maxdepth 3 | sed "s#$(echo ~/)##g"
 }
 
 move-repo() {
