16e81039012d38c17f858988b33af212a9dd2608
Koichi Miyamoto <m5813k@gmail.com>
2020-12-24 13:21:30 +0900

    pyenv

--- modules/anyenv/install.sh
+++ modules/anyenv/install.sh
@@ -1,2 +1,7 @@
 brew install anyenv
 anyenv install --init
+
+# for pyenv
+if [ "$(uname)" == "Linux" ]; then
+    sudo apt-get install gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev
+fi
