1ba7fc4d11d6423ee431fd0dd32a9e805b78f339
Koichi Miyamoto <m5813k@gmail.com>
2020-12-24 13:41:55 +0900

    install deps for python _ctypes not found error

--- modules/anyenv/install.sh
+++ modules/anyenv/install.sh
@@ -3,5 +3,5 @@ anyenv install --init
 
 # for pyenv
 if [ "$(uname)" == "Linux" ]; then
-    sudo apt-get install gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev
+    sudo apt-get install gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev libffi-dev
 fi
