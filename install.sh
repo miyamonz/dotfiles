3873c3905221aafd1b4cf5b143e6182bb27158f2
Koichi Miyamoto <m5813k@gmail.com>
2020-12-25 16:39:18 +0900

    fix

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ APPS=(
     tree
     shellcheck
     ffmpeg
+    graphviz
     hub
 )
 
--- modules/cloc/rc.sh
+++ modules/cloc/rc.sh
@@ -1,4 +1,5 @@
 CLOCKER="gocloc"
+alias cloc="gocloc"
 cloc-dir() {
     ls -d * | xargs -I% bash -c "$CLOCKER % | grep TOTAL | ( printf '%\t'; awk '{print \$5}')" | sort -rnk 2 | column -t
 }
