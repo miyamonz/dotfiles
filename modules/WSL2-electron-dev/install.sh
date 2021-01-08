97ce890957335d74b76f44f5236777b9de9ac204
Koichi Miyamoto <m5813k@gmail.com>
2021-01-08 14:18:45 +0900

    add deps for xserver

--- modules/WSL2-electron-dev/install.sh
+++ modules/WSL2-electron-dev/install.sh
@@ -1,3 +1,4 @@
 if IS_WSL; then
-    sudo apt-get install libxcursor1 libnss3-dev libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0　libgbm-dev libgconf-2-4
+    # for electron
+    sudo apt-get install -y libxcursor1 libnss3-dev libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libgconf-2-4 libasound2
 fi
