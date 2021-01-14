0e179c9b55a300a8614a2d6e30b3602783dca190
Koichi Miyamoto <m5813k@gmail.com>
2021-01-14 18:23:23 +0900

    fix tmux

--- modules/tmux/install.sh
+++ modules/tmux/install.sh
@@ -2,5 +2,4 @@ brew install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)"
-ln -snfv "$DIR/tmux" "$HOME/.tmux"
 ln -snfv "$DIR/tmux.conf" "$HOME/.tmux.conf"
