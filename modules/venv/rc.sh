9ad5cd0e82be82419b6d12ecfc3309ea8f785933
Koichi Miyamoto <m5813k@gmail.com>
2021-05-14 19:16:28 +0900

    fix venv

--- modules/venv/rc.sh
+++ modules/venv/rc.sh
@@ -1,1 +1,23 @@
-alias venv="source venv/bin/activate"
+#!/bin/bash
+
+function venv() {
+    case $1 in
+        init)
+            local CMD='python -m venv venv'
+            echo $CMD
+            eval $CMD
+            ;;
+        activate)
+            if [ ! -d "./venv" ]; then
+                echo 'venv not found. run `venv init`'
+                return 1 
+            fi 
+            local CMD='source venv/bin/activate'
+            echo $CMD
+            eval $CMD
+            ;;
+        *)
+            venv activate
+            ;;            
+    esac
+}
