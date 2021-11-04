eca594e105bac3a199140e392b270278b7901304
miyamonz <m5813k@gmail.com>
2021-11-04 14:50:12 +0900

    replace cd ghq

--- modules/ghq/rc.sh
+++ modules/ghq/rc.sh
@@ -1,17 +1,8 @@
 cat-repo() {
     find $(ghq root --all | xargs echo) -mindepth 3 -maxdepth 3 "$@"
 }
-move-recent-repo() {
-    cd ~/`cat-repo -printf "%T@ %p\n" | sed "s#$(echo ~/)##g" | sort -nr | cut -d ' ' -f2 | peco`
-}
-ghq-list() {
-    cat-repo | sed "s#$(echo ~/)##g"
-}
 
-move-repo() {
-    cd ~/$(ghq-list | peco)
-}
-
-alias r='move-repo'
+# see https://github.com/mollifier/anyframe/blob/master/anyframe-functions/sources/anyframe-source-ghq-repository
+alias r=anyframe-widget-cd-ghq-repository
 # フォルダの中身の更新日時もみたい
 alias rr='move-recent-repo'
