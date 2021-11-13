24ed309fd72a22e07ce3fdd583cad5b1001200a9
miyamonz <m5813k@gmail.com>
2021-11-13 21:57:27 +0900

    fix tmux on M1 mac

--- modules/tmux/install.sh
+++ modules/tmux/install.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/tmux-plugins/tpm $XDG_CONFIG_HOME/tmux/plugins/tpm
 
-DIR=${0:a:h}
+DIR=$HOME/dotfiles/modules/tmux
 ln -snfv "$DIR/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
