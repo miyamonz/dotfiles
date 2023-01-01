8ce52f8b93061d3724aacc6b2ebd3a4c51f2222d
miyamonz <m5813k@gmail.com>
2023-01-01 15:11:10 +0900

    fix

--- modules/ghq/rc.sh
+++ modules/ghq/rc.sh
@@ -1,8 +1,17 @@
 cat-repo() {
     find $(ghq root --all | xargs echo) -mindepth 3 -maxdepth 3 "$@"
 }
+move-recent-repo() {
+    cd ~/$(cat-repo -printf "%T@ %p\n" | sed "s#$(echo ~/)##g" | sort -nr | cut -d ' ' -f2 | peco)
+}
+ghq-list() {
+    cat-repo | sed "s#$(echo ~/)##g"
+}
 
-# see https://github.com/mollifier/anyframe/blob/master/anyframe-functions/sources/anyframe-source-ghq-repository
-alias r=anyframe-widget-cd-ghq-repository
+move-repo() {
+    cd ~/$(ghq-list | peco)
+}
+
+alias r='move-repo'
 # フォルダの中身の更新日時もみたい
 alias rr='move-recent-repo'
