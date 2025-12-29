94d34e8d9fd661440e01dd2540167310dbf70f51
miyamonz <m5813k@gmail.com>
2025-12-30 03:40:20 +0900

    fix cloc-dir

--- plugins/cloc-dir/cloc-dir.plugin.sh
+++ plugins/cloc-dir/cloc-dir.plugin.sh
@@ -4,7 +4,7 @@
 #    ls -d * | xargs -I% zsh -c "gocloc % | grep TOTAL | ( printf '%\t'; awk '{print \$5}')" | sort -rnk 2 | column -t
 # }
 cloc-dir() {
-    eza -1 --color never --git-ignore | \
-    xargs -I% zsh -c "gocloc % | grep TOTAL | ( printf '%\t'; awk '{print \$5}')" | \
+    fd -d 1 --type d --color never | \
+    xargs -I% zsh -c "scc --no-cocomo % | grep '^Total' | ( printf '%\t'; awk '{print \$5}')" | \
     sort -rnk 2 | column -t
 }
